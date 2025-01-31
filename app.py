d3130443678795876b00650bc56c389c43a88f88
Miki <104788529+Mikilled@users.noreply.github.com>
2025-02-01 04:09:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,25 @@ from pydantic import ValidationError, BaseModel
 from gptsearch import get_result
 
 app = Quart(__name__)
-semaphore = asyncio.Semaphore(7)
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+semaphore = None
+
+REQUESTS_PER_SECOND_LIMIT = 1.7
+DELAY_BETWEEN_REQUESTS = 1 / REQUESTS_PER_SECOND_LIMIT
+
 class PredictionRequest(BaseModel):
     id: int
+
+@app.before_serving
+async def setup():
+    global semaphore
+    semaphore = asyncio.BoundedSemaphore(4)
+
+async def limited_request_handling():
+    await asyncio.sleep(DELAY_BETWEEN_REQUESTS)
 
 @app.before_request
 async def log_request_info():
@@ -39,30 +50,24 @@ async def log_response_info(response):
 
 @app.route('/api/request', methods=['POST'])
 async def predict():
-    body = PredictionRequest.model_validate(await request.get_json())
-    try:
-        data = await request.get_json()
-        text = data.get("query")
-
-        if not text:
-            return jsonify({"error": "Поле 'query' обязательно"}), 400
-
-        logger.info(f"Processing request with text: {text}")
-
-
-        async with semaphore:
+    async with semaphore:
+        await limited_request_handling()
+        try:
+            body = PredictionRequest.model_validate(await request.get_json())
+            data = await request.get_json()
+            text = data.get("query")
+            if not text:
+                return jsonify({"error": "Поле 'query' обязательно"}), 400
+            logger.info(f"Processing request with text: {text}")
             result = await get_result(text, body.id)
-
-        return jsonify(result), 200
-
-    except ValidationError as e:
-        error_msg = e.json()
-        logger.error(f"Validation error: {error_msg}")
-        return jsonify({"error": error_msg}), 400
-    except Exception as e:
-        logger.error(f"Internal server error: {str(e)}")
-        return jsonify({"error": "Internal server error"}), 500
-
+            return jsonify(result), 200
+        except ValidationError as e:
+            error_msg = e.json()
+            logger.error(f"Validation error: {error_msg}")
+            return jsonify({"error": error_msg}), 400
+        except Exception as e:
+            logger.error(f"Internal server error: {str(e)}")
+            return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
